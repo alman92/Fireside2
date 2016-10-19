6274dfd35913965a993b545d3a30f4d1c761700f
Alex Manno <alman92@gmail.com>
2016-10-19 09:03:23 -0400

    created repo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,42 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @topic = Topic.find params[:topic_id]
+    @comment
   end
 
   def edit
+    @topic = Topic.find params[:topic_id]
     @comment = Comment.find(params[:id])
     if @comment.user == current_user
       @post.edit
     else
       flash[:alert] = "Only the author of the comment can edit"
     end
-    redirect_to comments_path
+    redirect_to topics_path
+  end
+
+  def update
+    @topic = Topic.find params[:topic_id]
+    @comment = Comment.find params [:id]
+    @comment.update(comment_params)
   end
 
   def create
+  @topic = Topic.find params[:topic_id]
   @comment = Comment.create!(comment_params.merge(user: current_user))
-  redirect_to comment_path(@comment)
+  redirect_to topic_path(@comment)
   end
 
   def destroy
+    @topic = Topic.find params [:topic_id]
     @comment = comment.find(params[:id])
     if @comment.user == current_user
       @comment.destroy
@@ -25,6 +44,11 @@ class CommentsController < ApplicationController
       flash[:alert] = "Only the author of the comment can delete"
     end
     redirect_to comments_path
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,41 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def show
+    @topic = Topic.find params[:id]
+  end
+
+  def edit
+    @topic = Topic.find params[:id]
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.create!(topic_params)
+
+    redirect_to topic_path(@topic)
+  end
+
+  def update
+    @topic = Topic.find params[:id]
+    @topic.update(topic_params)
+
+    redirect_to topic_path(@topic)
+  end
+
+  def destroy
+    @topic = Topic.find params[:id]
+    @topic.destroy
+
+    redirect_to topic_path
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:name)
+  end
 end
